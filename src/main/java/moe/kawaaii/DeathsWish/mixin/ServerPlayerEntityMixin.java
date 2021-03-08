c12028822960c8cb2408157b0e224527e4d34da0
EdgyCoder ✌ <administrator@kawaaii.moe>
2021-03-08 15:45:15 +0100

    Forgot to add credits to a fellow programmer!

--- src/main/java/moe/kawaaii/DeathsWish/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/moe/kawaaii/DeathsWish/mixin/ServerPlayerEntityMixin.java
@@ -11,6 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
 
+    /**
+     * Figured out via chronosacaria's code that I injected the code at the wrong spot.
+     * Thanks to them I was able to figure out why the armor did not copy over to the respawning player.
+     *
+     * https://www.curseforge.com/minecraft/mc-mods/save-gear-on-death
+     * Source: https://github.com/chronosacaria/SaveGearOnDeath/blob/main/src/main/java/chronosacaria/sgod/mixin/ServerPlayerEntityMixin.java
+     */
+
     @Inject(at = @At(value = "HEAD"), method = "copyFrom", cancellable = true)
     public void copyFrom(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
         if (!alive) {
